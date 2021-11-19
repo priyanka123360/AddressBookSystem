896cf0265593387d22ac25fceb81960550821c2a
priyankap <priyanka123360@gmail.com>
2021-11-19 17:32:33 +0530

    Modified : To Edit Contact in  Address Book.

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,10 +1,11 @@
 package com.bridgelabz;
 
 public class AddressBook {
-	public static void main(String[] args) {
+	public static void main(String[] args, String name) {
 		System.out.println("Welcome to Address Book System. ");
 		Contacts contacts = new Contacts();
-		contacts.addContact();
+		contacts.addContact(contacts);
+		contacts.editContacts(name, contacts);
 	}
 
 }
