a0f33db27dbbd29a999cd056f1aa637446528267
priyankap <priyanka123360@gmail.com>
2021-11-19 16:05:35 +0530

    Modified : To Add New Contact To Address Book.

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,7 +3,8 @@ package com.bridgelabz;
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System. ");
-	
+		Contacts contacts = new Contacts();
+		contacts.addContact();
 	}
 
 }
